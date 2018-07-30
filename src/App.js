a0b7f178d14ea3c27c11ed18a44aaf0bd7d14189
teustoscano <ssstoscano@gmail.com>
2018-07-29 23:17:34 -0300

    Redux boilerplate done and started actions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+
+import store from './store';
 import Posts from './components/Posts';
-import PostForm from './components/Postform'
+import PostForm from './components/Postform';
+
+
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +22,7 @@ class App extends Component {
         <hr/>
         <Posts />
       </div>
+      </Provider>
     );
   }
 }
